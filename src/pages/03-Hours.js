22efd5813cf04906813dc385bc0f58dcf7b79a49
Tom <Tom@MacBook-Air-2.local>
2025-04-11 23:03:29 -0700

    hi

--- src/pages/03-Hours.js
+++ src/pages/03-Hours.js
@@ -20,9 +20,9 @@ function Hours() {
           ['Tuesday', '7:30am-10:30am', '11:00am-2:00pm', '4:45pm-8:00pm', '9:00pm-11:00pm'],
           ['Wednesday', '7:30am-10:30am', '11:00am-2:00pm', '4:45pm-8:00pm', '9:00pm-11:00pm'],
           ['Thursday', '7:30am-10:30am', '11:00am-2:00pm', '4:45pm-8:00pm', '9:00pm-11:00pm'],
-          ['Friday', '7:30am-10:30am', '11:00am-2:00pm', '4:45pm-8:00pm', '9:00pm-11:00pm'],
+          ['Friday', '7:30am-10:30am', '11:00am-2:00pm', '4:45pm-8:00pm', 'CLOSED'],
           ['Saturday', '9:00am-1:30am', 'Dinner: 5:00pm-8:00pm', 'Late Night: CLOSED'],
-          ['Sunday', '9:00am-1:30am', 'Dinner: 5:00pm-8:00pm', 'Late Night: CLOSED']
+          ['Sunday', '9:00am-1:30am', 'Dinner: 5:00pm-8:00pm', 'Late Night: 9:00pm-11:00pm']
         ].map(([day, ...meals], index) => (
           <div key={index}>
             <h3 className="text-2xl font-bold mb-2">{day}</h3>

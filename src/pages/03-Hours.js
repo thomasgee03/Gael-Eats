7169b5651b1612c0f5860704bef53ca6404bbf9e
Tom <Tom@MacBook-Air-2.local>
2025-05-05 15:29:11 -0700

    ok

--- src/pages/03-Hours.js
+++ src/pages/03-Hours.js
@@ -33,7 +33,7 @@ function Hours() {
         <h2 className="text-3xl font-bold mb-4">Our Location</h2>
         <div className="w-full h-[450px] border border-black">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100935.18430647562!2d-122.18375612332176!3d37.761333630343316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f89bdaa162045%3A0x9046dac417308b15!2sOliver%20Hall!5e0!3m2!1sen!2sus!4v1745601040616!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3000.590362030672!2d-122.10967685014631!3d37.841034033542385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808f89bdaa162045%3A0x9046dac417308b15!2sOliver%20Hall!5e1!3m2!1sen!2sus!4v1746484038920!5m2!1sen!2sus"
             width="100%"
             height="100%"
             style={{ border: 0 }}
